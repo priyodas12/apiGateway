0e8e18cb55114764caaa9a8e279d75c7c0c8768b
Priyobrato Das <priyodas12@gmail.com>
2021-10-10 15:44:58 +0530

    eureka server

---
+++ src/test/java/io/microlab/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microlab.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
